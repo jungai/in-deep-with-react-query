b3f13f0f7ece6b146b047f0ada62197d7f7d3d27
jungai <juworaphol125@gmail.com>
2021-07-10 14:10:32 +0700

    feat: :sparkles: api get&post users

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,41 @@
+const users = [
+  {
+    id: 1,
+    name: 'iu'
+  },
+  {
+    id: 2,
+    name: 'rose'
+  },
+  {
+    id: 3,
+    name: 'sejeong'
+  },
+  {
+    id: 4,
+    name: 'yoona'
+  },
+  {
+    id: 5,
+    name: 'maibok❤️'
+  }
+]
+
+
 module.exports.setupAllRoutes = (e) => {
   e.get('/', (_req, res) => {
     res.json({ hello: 'world'})
+  })
+
+  e.get('/users', (_req, res) => {
+    res.json({ result: users})
+  })
+
+  e.post('/users', (req, res) => {
+    const { name } = req.body
+    users.push({ id: users.length + 1, name})
+
+    res.json({ success: true})
   })
 
   return e
