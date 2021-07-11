0ea63831959b1512aaee6f75cfb469574a28239a
jungai <juworaphol125@gmail.com>
2021-07-11 15:09:20 +0700

    test dependent query

--- client/src/components/WithNormalQuery.tsx
+++ client/src/components/WithNormalQuery.tsx
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { useUsersWithId } from '../data_services/users/get_users';
 
 function WithNormalQuery(): React.ReactElement {
-  const [id, setId] = useState(1);
+  const [id, setId] = useState<undefined | number>();
   const { data, isLoading, error, isFetching } = useUsersWithId(id);
 
   return (
     <div>
       <h1>With Normal Query</h1>
-      <button onClick={() => setId((prev) => prev + 1)}>id {id}</button>
+      <button onClick={() => setId((prev) => (prev ? prev + 1 : 1))}>
+        id {id}
+      </button>
       {isLoading ? (
         <p>loading ...</p>
       ) : error ? (
